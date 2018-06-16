dac49ade772815ae792a1b1e9a2df26404a8144a
ChengYiMingGit <cym_anhui@163.com>
2018-06-16 11:14:05 +0800

    this is c program ,name is test.c

---
+++ test.c
@@ -0,0 +1,24 @@
+
+#include	<stdio.h>
+#include	<stdlib.h>
+
+void	main(void)
+{
+	int a,b;
+	long long l =2.2;	//
+	miss_decl();
+	if(a == 0)
+	{
+		printf("a=%d",a);
+		printf("/r/n");
+	}
+	if(b !=0 )
+	{
+		printf("b=%d",b);
+		printf("/r/n");
+	}
+	printf("l = %f",l);
+	printf("/r/n");
+
+}
+
